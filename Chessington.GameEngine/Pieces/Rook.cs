7bcce92517582a1a52ce58ad58b07c25b9e05ab9
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-13 10:34:48 +0100

    Made Rooks able to move

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,22 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square mySquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+
+            for (int i = -GameSettings.BoardSize; i < GameSettings.BoardSize; i++)
+            {
+                if (mySquare.Row + i >= 0 && mySquare.Row + i < GameSettings.BoardSize)
+                {
+                    availableMoves.Add(new Square(mySquare.Row+i, mySquare.Col));
+                }
+                if (mySquare.Col + i >= 0 && mySquare.Col + i < GameSettings.BoardSize)
+                {
+                    availableMoves.Add(new Square(mySquare.Row, mySquare.Col+i));
+                }
+            }
+
+            return availableMoves;
         }
     }
 }
