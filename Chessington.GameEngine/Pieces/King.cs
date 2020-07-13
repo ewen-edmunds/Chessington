a44aecf3a51a49c368dc814c3bf7ec09607704d1
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-13 14:14:13 +0100

    Added Move Offset

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -5,12 +5,26 @@ namespace Chessington.GameEngine.Pieces
 {
     public class King : Piece
     {
+        protected static List<MoveOffset> MoveOffsets = new List<MoveOffset>()
+        {
+            new MoveOffset(-1,-1), new MoveOffset(-1,0), new MoveOffset(-1,1), 
+            new MoveOffset(0,-1), new MoveOffset(0,1),
+            new MoveOffset(1,-1), new MoveOffset(1,0), new MoveOffset(1,1)
+        };
         public King(Player player)
             : base(player) { }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square mySquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            
+            foreach (MoveOffset moveOffset in MoveOffsets)
+            {
+                availableMoves.Add(mySquare+moveOffset);
+            }
+            
+            return availableMoves.Where(IsWithinBounds);
         }
     }
 }
---
+++ Chessington.GameEngine/Pieces/MoveOffset.cs
@@ -0,0 +1,19 @@
+﻿namespace Chessington.GameEngine.Pieces
+{
+    public class MoveOffset
+    {
+        public int RowOffset;
+        public int ColOffset;
+
+        public MoveOffset(int rowOffset, int colOffset)
+        {
+            RowOffset = rowOffset;
+            ColOffset = colOffset;
+        }
+
+        public static Square operator +(Square square, MoveOffset offset)
+        {
+            return new Square(square.Row+offset.RowOffset, square.Col+offset.ColOffset);
+        }
+    }
+}
