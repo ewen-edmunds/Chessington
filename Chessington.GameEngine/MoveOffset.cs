0e80de27ac80b03c1c99b3ccec25a1cdf89d2f93
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-13 15:40:22 +0100

    Removed Unnecessary methods from MoveOffset

--- Chessington.GameEngine/MoveOffset.cs
+++ Chessington.GameEngine/MoveOffset.cs
@@ -22,42 +22,5 @@ namespace Chessington.GameEngine.Pieces
         {
             return new MoveOffset(offset.RowOffset*factor, offset.ColOffset*factor);
         }
-        
-        public static IEnumerable<MoveOffset> GetAllDiagonalOffsets()
-        {
-            HashSet<MoveOffset> diagonalOffsets = new HashSet<MoveOffset>();
-
-            for (int rowOffset = -1; rowOffset < 2; rowOffset+=2)
-            {
-                for (int colOffset = -1; colOffset < 2; colOffset+=2)
-                {
-                    for (int distance = 1; distance < GameSettings.BoardSize; distance++)
-                    {
-                        diagonalOffsets.Add(new MoveOffset(rowOffset * distance, colOffset * distance));
-                    }
-                }
-            }
-            
-            return diagonalOffsets;
-        }
-
-        public static IEnumerable<MoveOffset> GetAllLateralOffsets()
-        {
-            HashSet<MoveOffset> lateralOffsets = new HashSet<MoveOffset>();
-
-            for (int distance = 1; distance < GameSettings.BoardSize; distance++)
-            {
-                for (int rowDirection = -1; rowDirection < 2; rowDirection += 2)
-                {
-                    lateralOffsets.Add(new MoveOffset(rowDirection * distance, 0));
-                }
-                for (int colDirection = -1; colDirection < 2; colDirection += 2)
-                {
-                    lateralOffsets.Add(new MoveOffset(0, colDirection * distance));
-                }
-            }
-
-            return lateralOffsets;
-        }
     }
 }
