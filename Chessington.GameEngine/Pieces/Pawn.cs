0b62a71b99496bc7be3d9f7fe7a0353c482c878a
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-14 14:18:20 +0100

    Pawn refactoring

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -20,17 +20,20 @@ namespace Chessington.GameEngine.Pieces
             }
 
             availableMoves.Add(mySquare + new MoveOffset(moveDirection,0));
-            
-            if (!HasEverMoved)
-            {
-                if (BoardQuery.IsWithinBounds(mySquare + new MoveOffset(moveDirection, 0)) && board.GetPiece(mySquare + new MoveOffset(moveDirection, 0)) == null)
-                {
-                    availableMoves.Add(mySquare + new MoveOffset(moveDirection*2,0));
-                }
-            }
+            availableMoves.AddRange(GetSquaresFirstMove(board, moveDirection));
 
             availableMoves = availableMoves.Where(BoardQuery.IsWithinBounds).Where(square => board.GetPiece(square) == null).ToList();
 
+            availableMoves.AddRange(GetSquaresCanAttack(board, moveDirection));
+            
+            return availableMoves.Where(BoardQuery.IsWithinBounds);
+        }
+
+        private List<Square> GetSquaresCanAttack(Board board, int moveDirection)
+        {
+            Square mySquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            
             for (int colOffset =-1; colOffset < 2; colOffset+=2)
             {
                 Square captureSquare = mySquare + new MoveOffset(moveDirection, colOffset);
@@ -40,8 +43,24 @@ namespace Chessington.GameEngine.Pieces
                     availableMoves.Add(captureSquare);
                 }
             }
+
+            return availableMoves;
+        }
+
+        private List<Square> GetSquaresFirstMove(Board board, int moveDirection)
+        {
+            Square mySquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
             
-            return availableMoves.Where(BoardQuery.IsWithinBounds);
+            if (!HasEverMoved)
+            {
+                if (BoardQuery.IsWithinBounds(mySquare + new MoveOffset(moveDirection, 0)) && board.GetPiece(mySquare + new MoveOffset(moveDirection, 0)) == null)
+                {
+                    availableMoves.Add(mySquare + new MoveOffset(moveDirection*2,0));
+                }
+            }
+
+            return availableMoves;
         }
     }
 }
