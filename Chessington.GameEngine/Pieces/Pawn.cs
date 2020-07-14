dda192494551f6f905c846a56267d2d202498166
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-14 16:15:29 +0100

    Refactored Pawn

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -14,11 +14,7 @@ namespace Chessington.GameEngine.Pieces
             Square mySquare = board.FindPiece(this);
             List<Square> availableMoves = new List<Square>();
             
-            int moveDirection = 1;
-            if (Player == Player.White)
-            {
-                moveDirection = -1;
-            }
+            int moveDirection = Player == Player.White ? -1 : 1;
 
             availableMoves.Add(mySquare + new MoveOffset(moveDirection,0));
             availableMoves.AddRange(GetSquaresFirstMove(board, moveDirection));
@@ -30,7 +26,20 @@ namespace Chessington.GameEngine.Pieces
             
             return availableMoves.Where(BoardQuery.IsWithinBounds);
         }
+        
+        private List<Square> GetSquaresFirstMove(Board board, int moveDirection)
+        {
+            Square mySquare = board.FindPiece(this);
+            List<Square> availableMoves = new List<Square>();
+            
+            if (!HasEverMoved && BoardQuery.IsWithinBounds(mySquare + new MoveOffset(moveDirection, 0)) 
+                              && board.GetPiece(mySquare + new MoveOffset(moveDirection, 0)) == null)
+            {
+                availableMoves.Add(mySquare + new MoveOffset(moveDirection*2,0));
+            }
 
+            return availableMoves;
+        }
         private List<Square> GetSquaresCanAttack(Board board, int moveDirection)
         {
             Square mySquare = board.FindPiece(this);
@@ -48,23 +57,6 @@ namespace Chessington.GameEngine.Pieces
 
             return availableMoves;
         }
-
-        private List<Square> GetSquaresFirstMove(Board board, int moveDirection)
-        {
-            Square mySquare = board.FindPiece(this);
-            List<Square> availableMoves = new List<Square>();
-            
-            if (!HasEverMoved)
-            {
-                if (BoardQuery.IsWithinBounds(mySquare + new MoveOffset(moveDirection, 0)) && board.GetPiece(mySquare + new MoveOffset(moveDirection, 0)) == null)
-                {
-                    availableMoves.Add(mySquare + new MoveOffset(moveDirection*2,0));
-                }
-            }
-
-            return availableMoves;
-        }
-        
         private List<Square> GetSquaresEnPassant(Board board, int moveDirection)
         {
             Square mySquare = board.FindPiece(this);
@@ -72,19 +64,17 @@ namespace Chessington.GameEngine.Pieces
 
             for (int colOffset =-1; colOffset < 2; colOffset+=2)
             {
-                Square captureSquare = mySquare + new MoveOffset(0, colOffset);
-                if (BoardQuery.IsWithinBounds(captureSquare) &&
-                    BoardQuery.IsOppositeColourOnSquares(board, mySquare, captureSquare))
+                Square sidewaysSquare = mySquare + new MoveOffset(0, colOffset);
+                if (BoardQuery.IsWithinBounds(sidewaysSquare) &&
+                    BoardQuery.IsOppositeColourOnSquares(board, mySquare, sidewaysSquare))
                 {
-                    if (board.previousBoard.GetPiece(captureSquare + new MoveOffset(moveDirection * 2, 0)) ==
-                        board.GetPiece(captureSquare))
+                    if (board.previousBoard.GetPiece(sidewaysSquare + new MoveOffset(moveDirection * 2, 0)) == board.GetPiece(sidewaysSquare))
                     {
-                        availableMoves.Add(captureSquare + new MoveOffset(moveDirection,0));
+                        availableMoves.Add(sidewaysSquare + new MoveOffset(moveDirection,0));
                     }
                     
                 }
             }
-
             return availableMoves;
         }
     }
